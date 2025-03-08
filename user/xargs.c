2fc87804e46eaff254de9edb83252d0667cdf7d9
shimmer <1072215361@qq.com>
2025-03-08 23:15:45 +0800

    Lab: Xv6 and Unix utilities---xargs complete

---
+++ user/xargs.c
@@ -0,0 +1,34 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+int
+main(int argc, char *argv[])
+{
+    char t;
+    char q[1024];
+    char *p = q;
+    char *v[MAXARG];
+
+    while(read(0, &t, sizeof(char))) {
+        if (t == '\n') {
+            for (int i = 1, j = 0; i < argc + 1; i++, j++) {
+                if (i < argc) {
+                    v[j] = argv[i];
+                } else {
+                    v[j] = q;
+                }
+            }
+            int pid = fork();
+            if (pid == 0) {
+                exec(argv[1], v);
+            }
+            wait(0);
+            p = "";
+        } else {
+            *p++ = t;
+        }
+    }
+    exit(0);
+}
